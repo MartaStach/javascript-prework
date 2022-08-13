aeec49ec2f738f954a8e1ad5c25431834fc55f19
Marta Stach <marta.m.stach@gmail.com>
2022-08-13 18:28:07 +0200

    computerMove script added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+var computerMove;
+computerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
