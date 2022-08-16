e8261fab45f456048e56548423d1c85cf245ac0c
Marta Stach <marta.m.stach@gmail.com>
2022-08-16 04:13:11 +0200

    styling rock, paper, scissors; saper added

--- js/functions.js
+++ js/functions.js
@@ -4,15 +4,28 @@ function printMessage(msg){
 	document.getElementById('messages').appendChild(div);
 }
 
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
 function printResults(results){
 	var div = document.createElement('div');
 	div.innerHTML = results;
 	document.getElementById('results').appendChild(div);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
 function clearResults(){
 	document.getElementById('results').innerHTML = '';
 }
+
+
+function putBombs (bomblocation){
+	var th = document.createElement('th');
+	th.innerHTML = results;
+	document.getElementById(bomblocation);
+	document.getElementById(bomblocation).innerHTML = 'X';
+  }
+
+  function clearBombs (bomblocation){
+	document.getElementById(bomblocation).innerHTML = '';
+  }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest, set;
+var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest, buttonReset, buttonSaper, set;
 
 
 buttonRock = document.getElementById('button-rock');
 buttonPaper = document.getElementById('button-paper');
 buttonScissors = document.getElementById('button-scissors');
+buttonReset = document.getElementById('button-reset');
+buttonSaper = document.getElementById('button-saper');
+buttonBomb=document.getElementById('bomb');
+
 
 
 
@@ -14,16 +18,30 @@ function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
   playerMove = argButtonName;
-  console.log('ruch gracza to: ' + playerMove);
-  randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('wylosowana liczba to: ' + randomNumber);
-  computerMove = getMoveName(randomNumber);
-  console.log('ruch komputera to: ' + computerMove);
-  clearResults();
-  displayResult(playerMove, computerMove);
-}
+    console.log('ruch gracza to: ' + playerMove);
+    randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('wylosowana liczba to: ' + randomNumber);
+    computerMove = getMoveName(randomNumber);
+    console.log('ruch komputera to: ' + computerMove);
+    printMessage('Ruch komputera to ' + computerMove);
+    clearResults();
+    displayResult(playerMove, computerMove);
+  }
+
+/**
+ * Describe this function...
+ */
+  function buttonResetClicked(_buttonReset) {
+    console.log(argButtonName + 'został kliknięty')
+    console.log( 'wyniki zostały zresetowane');
+    clearMessages();
+    clearResults();
+    playerResult = 0;
+    computerResult = 0;
+  }
 
 var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
+
 
 /**
  * Describe this function...
@@ -59,7 +77,7 @@ function displayResult(argPlayerMove, argComputerMove) {
   } else if (argPlayerMove == argComputerMove) {
     printMessage('Remis');
   } else {
-    printMessage('Przegrywasz :(');
+    printMessage('Przegrywasz');
     computerResult=computerResult + 1
   }
   printResults(playerResult + '-' + computerResult)
@@ -70,13 +88,144 @@ var playerResult, computerResult
 playerResult=0;
 computerResult=0;
 
-/**
- * Describe this function...
- */
 
+//saper
+var buttonSaper, argIdName
+
+function buttonSaperClicked () {
+  firstBombNumber = 0;
+  secondBombNumber = 0;
+  thirdBombNumber = 0;
+  clearMessages();
+  mixBombNumbers();
+  GuessBomb();
+ 
+  } 
+  
+
+
+function nextGuesses () {
+  clearMessages ();
+  GuessBomb () = playerMoveSaper
+  playersChoiceChecking (playerMoveSaper);
+  }
+
+function mixBombNumbers() {
+  console.log('1');
+  firstBombNumber  = Math.floor(Math.random() * 9 + 1);
+  console.log('Numer pierwszej bomby to ' + firstBombNumber);
+  secondBombNumber = Math.floor(Math.random() * 9 + 1);
+  while (secondBombNumber == firstBombNumber) {
+    secondBombNumber = Math.floor(Math.random() * 9 + 1);
+    console.log('Ponowne losowanie drugiej liczby');
+  }
+  console.log('Numer drugiej bomby to ' + secondBombNumber);
+  thirdBombNumber = Math.floor(Math.random() * 9 + 1);
+  while (thirdBombNumber == firstBombNumber || thirdBombNumber == secondBombNumber) {
+    thirdBombNumber = Math.floor(Math.random() * 9 + 1);
+    console.log('Ponowne losowanie trzeciej liczby');
+  }
+  console.log('Numer trzeciej bomby to ' + thirdBombNumber);
+  //putBombs (getIdName(firstBombNumber));
+  //putBombs (getIdName(secondBombNumber));
+  //putBombs (getIdName(thirdBombNumber));
+  console.log('Położenie bomb to: ' + getIdName(firstBombNumber) +', ' + getIdName(secondBombNumber) + ', ' +  getIdName(thirdBombNumber));
+  //console.log(putBombs (getIdName(firstBombNumber, secondBombNumber, thirdBombNumber)));
+}
+
+function getIdName(bombnumber) {
+  if (bombnumber == 1) {
+    return 'a1';
+  } else if (bombnumber == 2) {
+    return 'a2';
+  } else if (bombnumber == 3) {
+    return 'a3';
+  } else if (bombnumber == 4) {
+    return 'b1';
+  } else if (bombnumber == 5) {
+    return 'b2';
+  } else if (bombnumber == 6) {
+    return 'b3';
+  } else if (bombnumber == 7) {
+    return 'c1';
+  } else if (bombnumber == 8) {
+    return 'c2';
+  } else if (bombnumber == 9) {
+    return 'c3';
+  }
+}
+function GuessBomb () {
+  playerMoveSaper = prompt('Wybierz pole, na którym nie spodziewasz się bomby: 1-a1, 2-a2, 3-a3, 4-b1, 5-b2, 6-b3, 7-c1, 8-c2, 9-c3. Wpisz numer pola:');
+  getIdName(playerMoveSaper);
+  console.log('Gracz wybrał pole ' + playerMoveSaper + 'czyli' + getIdName(playerMoveSaper));
+  playersChoiceChecking (playerMoveSaper);
+  choice = playersChoiceChecking
+}
+
+
+function playersChoiceChecking (playerMoveSaper) {
+  if (playerMoveSaper == firstBombNumber || playerMoveSaper == secondBombNumber || playerMoveSaper == thirdBombNumber) {
+    printMessage('Przegrałeś, naciśnij bombę i zagraj jeszcze raz');
+    firstBombNumber = 0;
+    secondBombNumber = 0;
+    thirdBombNumber = 0;
+  } else {
+    NextMove = prompt('Dobrze, graj dalej! Wybierz pole, na którym nie spodziewasz się bomby: 1-a1, 2-a2, 3-a3, 4-b1, 5-b2, 6-b3, 7-c1, 8-c2, 9-c3. Wpisz numer pola:');
+    getIdName(NextMove);
+      if (NextMove == firstBombNumber || NextMove == secondBombNumber || NextMove == thirdBombNumber) {
+        BadChoice ();
+      } else {
+        NextMove = prompt('Dobrze, graj dalej! Wybierz pole, na którym nie spodziewasz się bomby: 1-a1, 2-a2, 3-a3, 4-b1, 5-b2, 6-b3, 7-c1, 8-c2, 9-c3. Wpisz numer pola:');
+        getIdName(NextMove);
+        if (NextMove == firstBombNumber || NextMove == secondBombNumber || NextMove == thirdBombNumber) {
+          BadChoice ();
+        } else {
+          NextMove = prompt('Dobrze, graj dalej! Wybierz pole, na którym nie spodziewasz się bomby: 1-a1, 2-a2, 3-a3, 4-b1, 5-b2, 6-b3, 7-c1, 8-c2, 9-c3.Wpisz numer pola:');
+          getIdName(NextMove);
+          if (NextMove == firstBombNumber || NextMove == secondBombNumber || NextMove == thirdBombNumber) {
+            BadChoice ();
+          } else {
+            NextMove = prompt('Dobrze, graj dalej! Wybierz pole, na którym nie spodziewasz się bomby: 1-a1, 2-a2, 3-a3, 4-b1, 5-b2, 6-b3, 7-c1, 8-c2, 9-c3. Wpisz numer pola:');
+            getIdName(NextMove);
+            if (NextMove == firstBombNumber || NextMove == secondBombNumber || NextMove == thirdBombNumber) {
+              BadChoice ();
+            } else {
+              NextMove = prompt('Dobrze, graj dalej! Wybierz pole, na którym nie spodziewasz się bomby: 1-a1, 2-a2, 3-a3, 4-b1, 5-b2, 6-b3, 7-c1, 8-c2, 9-c3. Wpisz numer pola:');
+              getIdName(NextMove);
+              if (NextMove == firstBombNumber || NextMove == secondBombNumber || NextMove == thirdBombNumber) {
+                BadChoice ();
+              } else {
+                printMessage('Wygrałeś!');
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+
+function BadChoice () {
+  printMessage('Przegrałeś, naciśnij bombę i zagraj jeszcze raz');
+  firstBombNumber = 0;
+  secondBombNumber = 0;
+  thirdBombNumber = 0;
+}
+function NextLevel () {
+  if (playerMoveSaper == firstBombNumber || playerMoveSaper == secondBombNumber || playerMoveSaper == thirdBombNumber) {
+
+  } else {
+    GuessBomb ();
+  }
+}
 
 
 //buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
 buttonRock.addEventListener('click', function () { buttonClicked('kamień'); });
 buttonPaper.addEventListener('click', function () { buttonClicked('papier'); });
 buttonScissors.addEventListener('click', function () { buttonClicked('nożyce'); });
+buttonReset.addEventListener('click', function () { buttonResetClicked('reset'); });
+buttonSaper.addEventListener('click', function () { buttonSaperClicked('saper'); });
+
+
