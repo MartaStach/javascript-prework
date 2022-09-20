6bac88d830619892febc10382d090c1c50842a43
MartaStach <marta.m.stach@gmail.com>
2022-09-20 20:47:12 +0200

    deleted all console.logs, deleted comments, else from getMoveName deleted

--- js/script.js
+++ js/script.js
@@ -1,79 +1,61 @@
 
 let playerResult, computerResult, randomNumber, playerMove, computerMove;
 const buttonRock = document.getElementById('button-rock'),
-buttonPaper = document.getElementById('button-paper'),
-buttonScissors = document.getElementById('button-scissors'),
-buttonReset = document.getElementById('button-reset');
+  buttonPaper = document.getElementById('button-paper'),
+  buttonScissors = document.getElementById('button-scissors'),
+  buttonReset = document.getElementById('button-reset');
 
-/**
- * Describe this function...
- */
+
 
 function buttonClicked(playerMove) {
   clearMessages();
-  console.log(playerMove + ' został kliknięty');
-    console.log('ruch gracza to: ' + playerMove);
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('wylosowana liczba to: ' + randomNumber);
-    let computerMove = getMoveName(randomNumber);
-    console.log('ruch komputera to: ' + computerMove);
-    printMessage('Ruch komputera to ' + computerMove);
-    clearResults();
-    displayResult(playerMove, computerMove);
-  }
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerMove = getMoveName(randomNumber);
+  printMessage('Ruch komputera to ' + computerMove);
+  clearResults();
+  displayResult(playerMove, computerMove);
+}
 
-/**
- * Describe this function...
- */
-  function buttonResetClicked(buttonReset) {
-    console.log(buttonReset + 'został kliknięty')
-    console.log( 'wyniki zostały zresetowane');
-    clearMessages();
-    clearResults();
-    playerResult = 0;
-    computerResult = 0;
-  }
 
-/**
- * Describe this function...
- */
+function buttonResetClicked(buttonReset) {
+  clearMessages();
+  clearResults();
+  playerResult = 0;
+  computerResult = 0;
+}
+
 
 function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
   if (argMoveId == 1) {
     return 'kamień';
   } else if (argMoveId == 2) {
     return 'papier';
   } else if (argMoveId == 3) {
     return 'nożyce';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-    return 'kamień';
   }
+  printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+  return 'kamień';
 }
 
-/**
- * Describe this function...
- */
-  playerResult=0;
-  computerResult=0;
+
+playerResult = 0;
+computerResult = 0;
 
 function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
     printMessage('Wygrywasz!');
-    playerResult=playerResult + 1
+    playerResult = playerResult + 1
   } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
     printMessage('Wygrywasz!');
-    playerResult=playerResult + 1
+    playerResult = playerResult + 1
   } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
     printMessage('Wygrywasz!');
-    playerResult=playerResult + 1
+    playerResult = playerResult + 1
   } else if (argPlayerMove == argComputerMove) {
     printMessage('Remis');
   } else {
     printMessage('Przegrywasz');
-    computerResult=computerResult + 1
+    computerResult = computerResult + 1
   }
   printResults(playerResult + '-' + computerResult)
 }
